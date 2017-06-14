6e30f11dc2da7aa4628fda1d2c8131c7d5ef5801
winner <wuyinyin163@163.com>
2017-06-14 10:03:27 +0800

    vue pc 分页

---
+++ example/news/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <News></News>
+  </div>
+</template>
+
+<script>
+import News from './components/News'
+
+export default {
+  name: 'app',
+  components: {
+    News
+  }
+}
+</script>
+
+<style>
+
+</style>
